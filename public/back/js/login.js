7f674304ce22b8958b5271a555e65decff44107a
zhouda123 <819350199@qq.com>
2019-09-06 21:46:39 +0800

    完成的项目

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -27,6 +27,10 @@ $(function() {
             message: '用户名长度必须是2-6位',
             min: 2,
             max: 6
+          },
+          //callback 专门用来定制回调的提示内容的
+          callback: {
+            message: '用户名不存在'
           }
         }
       },
@@ -42,6 +46,9 @@ $(function() {
             min: 6,
             max: 12,
             message: '密码长度必须是6-12位'
+          },
+          callback: {
+            message: '密码错误'
           }
         }
       }
@@ -67,10 +74,17 @@ $(function() {
           location.href = 'index.html'
         }
         if (info.error === 1000) {
-          alert('用户名不存在')
+          // alert('用户名不存在')
+          // 调用实例的更新校验方法 updateStatus 将校验状态更新失败
+          $('#form')
+            .data('bootstrapValidator')
+            .updateStatus('username', 'INVALID', 'callback')
         }
         if (info.error === 1001) {
-          alert('密码错误')
+          // alert('密码错误')
+          $('#form')
+            .data('bootstrapValidator')
+            .updateStatus('password', 'INVALID', 'callback')
         }
       }
     })
