2ddf05e840a5a13db11c223c969e0f913fdd7299
zhouda123 <819350199@qq.com>
2019-09-07 12:42:33 +0800

    进度条完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,25 @@
 /*需求:在第一个啊ajax发送到时候,开启进度条
-在全部啊ajax回来到时候 关闭进度条 */
+在全部啊ajax回来的时候关闭进度条 */
 // 开启进度条
-NProgress.start()
+// NProgress.start()
+// setTimeout(function() {
+//   NProgress.done()
+// }, 2000)
+// ajax 全局事件
+//  .ajaxComplete（）   当ajax完成时,调用  不管成不成功
+// .ajaxError（） 失败调用
+// .ajaxSuccess（） 成功调用
+/*
+    .ajaxSend() 当ajax发送前调用
+    .ajaxStart() 当第一个ajax发送时调用
+    .ajaxStop() 当全部的ajax请求完成时调用
+
+ */
+$(document).ajaxStart(function() {
+  //第一个ajax发送时 开启进度条
+  NProgress.start()
+})
+$(document).ajaxStop(function() {
+  //第一个ajax发送时 开启进度条
+  NProgress.done()
+})
